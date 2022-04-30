cfceda9a992594775c719cd7b4f65aac86167820
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-30 11:19:47 +0600

    Banner added

---
+++ src/Pages/Shared/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner-container'>
+
+            <div className='heading'>
+
+                <h1 className='heading-title'>Square Inventory</h1>
+
+            </div>
+
+            <img className='banner-img' src="https://i.ibb.co/YhSbcvw/warehouse.png" alt="" />
+
+
+
+
+
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -20,20 +20,20 @@ const Header = () => {
 
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="home#services">Services</Nav.Link>
-                            <Nav.Link href="home#experts">Experts</Nav.Link>
+                            <Nav.Link href="home#services"><span className='manu-style'>Services</span></Nav.Link>
+
 
                         </Nav>
 
                         <Nav>
 
 
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/about"><span className='manu-style'>Services</span></Nav.Link>
 
-                            <Nav.Link as={Link} to="/addservice">Add</Nav.Link>
+                            <Nav.Link as={Link} to="/addservice"><span className='manu-style'>Services</span></Nav.Link>
 
-                            <Nav.Link as={Link} to="/manage">Manage</Nav.Link>
-                            <Nav.Link as={Link} to="/orders">orders</Nav.Link>
+                            <Nav.Link as={Link} to="/manage"><span className='manu-style'>Services</span></Nav.Link>
+
 
 
                             <button type="button" className="btn btn-outline-success">Success</button>
