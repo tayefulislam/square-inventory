692ef0b2e33a7058d2e0380289f12899157cf86f
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-01 16:49:52 +0600

    Sign Up function added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -11,29 +11,70 @@ const Login = () => {
 
 
 
+    const handeLogin = (event) => {
+        event.preventDefault()
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        console.log(name)
+
+
+    }
+
+
 
 
     return (
-        <div>
-
-            <h2>Login</h2>
 
 
-            <div>
-                <form >
+        <div className='container mt-4 form'>
 
-                    <input type="email" placeholder='Email Address' />
-                    <input type="password" placeholder='Password' />
+            <h1 className='text-center mb-5 mt-4 text-light'>Login</h1>
 
 
-                </form>
+            <form onSubmit={handeLogin}>
 
-                <button onClick={() => signInWithGoogle()} >Google</button>
-                <button onClick={() => naviagte('/signup')} >Signup</button>
-            </div>
+
+                <div className="form-floating mb-3 w-50 mx-auto">
+                    <input type="email" className="form-control" name='email' id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Email address</label>
+                </div>
+
+                <div className="form-floating w-50 mx-auto">
+                    <input type="password" className="form-control"
+                        name='password' id="floatingPassword" placeholder="Password" />
+                    <label for="floatingPassword">Password</label>
+                </div>
+
+
+
+                <div className="d-grid gap-2 mt-3">
+                    <button className="btn btn-outline-dark w-20 mx-auto" type="submit"><span className='text-light click-btn'>Login</span></button>
+
+
+
+                </div>
+            </form>
+
+
+
+
+            <button onClick={() => signInWithGoogle()} >Google</button>
+            <button onClick={() => naviagte('/signup')} >Signup</button>
 
 
         </div>
+
+
+
+
+
+
+
+
+
     );
 };
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { useAuthState, useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useAuthState, useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 import './Register.css'
 
@@ -13,30 +14,72 @@ const Register = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
 
+
+
+
+    const [updateProfile, updating, errorProfile] = useUpdateProfile(auth);
+
+
+
+    const handeSubmit = async (event) => {
+        event.preventDefault()
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+
+        await createUserWithEmailAndPassword(email, password)
+
+        await updateProfile({ displayName: name });
+
+
+
+
+        if (error) {
+
+            console.log(error.message)
+
+
+        }
+
+        if (user) {
+            toast('Sign Up complete')
+            event.target.reset()
+        }
+
+
+
+
+
+        console.log(name)
+    }
+
     return (
         <div className='container mt-4 form'>
 
             <h1 className='text-center mb-5 mt-4 text-light'>Registration</h1>
 
 
-            <form >
+            <form onSubmit={handeSubmit}>
 
-                <div class="form-floating mb-3 w-50 mx-auto">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="Your Name" />
-                    <label for="floatingInput">Your Name</label>
+                <div className="form-floating mb-3 w-50 mx-auto">
+                    <input type="text" className="form-control" name='name' id="floatingInput" placeholder="Your Name" />
+                    <label for="floatingInput">Your Name </label>
                 </div>
-                <div class="form-floating mb-3 w-50 mx-auto">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                <div className="form-floating mb-3 w-50 mx-auto">
+                    <input type="email" className="form-control" name='email' id="floatingInput" placeholder="name@example.com" />
                     <label for="floatingInput">Email address</label>
                 </div>
 
-                <div class="form-floating w-50 mx-auto">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                <div className="form-floating w-50 mx-auto">
+                    <input type="password" className="form-control"
+                        name='password' id="floatingPassword" placeholder="Password" />
                     <label for="floatingPassword">Password</label>
                 </div>
 
-                <div class="d-grid gap-2 mt-3">
-                    <button class="btn btn-outline-dark w-20 mx-auto" type="button"><span className='text-light register-btn'>SignUp</span></button>
+                <div className="d-grid gap-2 mt-3">
+                    <button className="btn btn-outline-dark w-20 mx-auto" type="submit"><span className='text-light click-btn'>SignUp</span></button>
 
 
 
